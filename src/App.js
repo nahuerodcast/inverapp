19b8ff55eec3d622d8f4fb249a1b9db42f5fbb2d
nahue rodcast <nahuerodcast@gmail.com>
2021-06-16 18:47:30 -0300

    commit on 'main'

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          asd <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -19,7 +19,9 @@ function App() {
         </a>
       </header>
     </div>
+  
   );
+  
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Navbar from './components/NavBar';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Navbar />
   </React.StrictMode>,
   document.getElementById('root')
 );
