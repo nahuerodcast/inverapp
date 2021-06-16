0c49f4519532fb20331fc68b5498f750ab15a5f5
nahue rodcast <nahuerodcast@gmail.com>
2021-06-16 19:07:00 -0300

    commit on 'main'

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,29 +10,31 @@ import Button from 'react-bootstrap/Button'
 function nave() {
   return (
     <div className="Navbar">
-      <header className="App-header">
         <Navbar bg="light" expand="lg">
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">Inverapp</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
               <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <Nav.Link href="#link">Estado de cuenta</Nav.Link>
+              <Nav.Link href="#link">Estado de órdenes</Nav.Link>
+              <Nav.Link href="#link">Invertir</Nav.Link>
+              <NavDropdown title="Mi cuenta" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Invertir</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.1">Cargar Saldo</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.1">Extraer fondos</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Configuración</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Perfil</NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.4">Salir</NavDropdown.Item>
               </NavDropdown>
             </Nav>
             <Form inline>
-              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-              <Button variant="outline-success">Search</Button>
+              <FormControl type="text" placeholder="Buscar" className="mr-sm-2" />
+              <Button variant="outline-success">Buscar</Button>
             </Form>
           </Navbar.Collapse>
         </Navbar>
-      </header>
     </div>
 
   );
