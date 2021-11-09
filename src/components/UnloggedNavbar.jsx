0eafaba60aa2b3d8b4618d435f507d54d22db407
Nahue <nahuerodcast@gmail.com>
2021-11-09 20:11:09 -0300

    updating navbar

--- src/components/UnloggedNavbar.jsx
+++ src/components/UnloggedNavbar.jsx
@@ -3,6 +3,7 @@ import { useColorMode } from "@chakra-ui/color-mode";
 import { Flex, Heading } from "@chakra-ui/layout";
 import { Button } from "@chakra-ui/button";
 import { WiMoonAltWaningCrescent2 } from "react-icons/wi";
+import { IoIosArrowDown } from "react-icons/io";
 
 export const UnloggedNavbar = () => {
   const { colorMode, toggleColorMode } = useColorMode();
@@ -16,15 +17,89 @@ export const UnloggedNavbar = () => {
         p={4}
         boxShadow="lg"
       >
-        <Heading fontWeight="extrabold">Inverapp</Heading>
-        <Button
-          onClick={toggleColorMode}
-          variant="ghost"
-          p={0}
-          borderRadius={9999}
-        >
-          <WiMoonAltWaningCrescent2 size={20} />
-        </Button>
+        <Flex flexDir="row" alignItems="center" justifyContent="center">
+          <Heading
+            as="h1"
+            fontWeight="extrabold"
+            fontFamily="Inter,sans-serif"
+            mr={10}
+          >
+            Inverapp
+          </Heading>
+          <Heading
+            as="h5"
+            color="gray.400"
+            fontWeight="medium"
+            fontSize={20}
+            pr={4}
+          >
+            <Flex
+              flexDir="row"
+              fontFamily="Inter,sans-serif"
+              alignItems="center"
+            >
+              Invertí <IoIosArrowDown />
+            </Flex>
+          </Heading>
+          <Heading
+            as="h5"
+            color="gray.400"
+            fontWeight="medium"
+            fontSize={20}
+            pr={4}
+          >
+            <Flex
+              flexDir="row"
+              fontFamily="Inter,sans-serif"
+              alignItems="center"
+            >
+              Nosotros <IoIosArrowDown />
+            </Flex>
+          </Heading>
+          <Heading
+            as="h5"
+            color="gray.400"
+            fontFamily="Inter,sans-serif"
+            fontWeight="medium"
+            fontSize={20}
+            pr={4}
+          >
+            <Flex
+              flexDir="row"
+              fontFamily="Inter,sans-serif"
+              alignItems="center"
+            >
+              Aranceles <IoIosArrowDown />
+            </Flex>
+          </Heading>
+        </Flex>
+        <Flex alignItems="center">
+          <Button
+            onClick={toggleColorMode}
+            variant="ghost"
+            p={0}
+            borderRadius={9999}
+            color="gray.500"
+          >
+            <WiMoonAltWaningCrescent2 size={20} />
+          </Button>
+          <Button
+            colorScheme="teal"
+            variant="solid"
+            fontFamily="Inter,sans-serif"
+            m={2}
+          >
+            Abrir cuenta
+          </Button>
+          <Button
+            colorScheme="teal"
+            variant="outline"
+            fontFamily="Inter,sans-serif"
+            m={1}
+          >
+            Ingresar
+          </Button>
+        </Flex>
       </Flex>
     </div>
   );
