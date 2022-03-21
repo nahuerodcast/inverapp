4df517a45c2784d1cc184622b5e9c4fa58fea921
Nahue Rodcast <nahuerodcast@gmail.com>
2022-03-21 20:01:17 -0300

    first commit

--- src/components/global/LoginProvider.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { LoggedContainer } from "../../containers/LoggedContainer";
-import { UnloggedContainer } from "../../containers/UnloggedContainer";
-import { useAuth } from "../../contexts/AuthContext";
-
-export const LoginProvider = () => {
-  const { currentUser } = useAuth();
-  return <>{!currentUser ? <UnloggedContainer /> : <LoggedContainer />}</>;
-};
