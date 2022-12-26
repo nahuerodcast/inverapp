a4bb0e90cbbc2a55b363ba5d84eb5d07f2811ee9
Nahuel Rodriguez <nahuerodcast@gmail.com>
2022-12-26 20:50:38 +0000

    fixing portfolio reduce method

--- src/components/logged/Portfolio.jsx
+++ src/components/logged/Portfolio.jsx
@@ -56,36 +56,37 @@ export const Portfolio = ({ arrayPortfolio }) => {
     });
   };
 
-  let totalPortfolio = arrayPortfolio.reduce(
-    (a, b) => a + b.price * b.quantity,
-    0
-  );
-
   const [newArray, setNewArray] = useState(arrayPortfolio);
 
-  const basado = newArray;
-
-  const arey = [
-    { id: 1, price: 10 },
-    { id: 2, price: 20 },
-    { id: 3, price: 30 },
-  ];
-  let sum = arey.reduce((a, b) => a + b.price, 0);
-  let avg = sum / arey.length;
-  // console.log(avg);
+   let totalPortfolio = newArray.reduce(
+    (a, b) => a + b.currentTotal,
+    0
+  );
 
   useEffect(() => {
     const newPortfolio = arrayPortfolio.reduce((acc, currentVal) => {
       const elementAlreadyExists = acc.find(
         (element) => element.symbol === currentVal.symbol
       );
+      const filteredPrice = price.filter((p) =>
+              p.name.includes(currentVal.name)
+            );
+            
+      currentVal.currentTotal = currentVal.price * currentVal.quantity
+      
       if (elementAlreadyExists) {
         return acc.map((element, i, arr) => {
+          const filteredArray = arrayPortfolio.filter(
+            (element) => element.symbol === currentVal.symbol
+          );
           if (element.symbol === currentVal.symbol) {
             return {
               ...element,
-              price: arr.reduce((a, b) => a + b.price, 0),
+              price:
+                filteredArray.reduce((a, b) => a + b.price, 0) /
+                filteredArray.length,
               quantity: element.quantity + currentVal.quantity,
+              currentTotal: filteredArray.reduce((a, b) => a + filteredPrice[0]?.current_price * b.quantity, 0)
             };
           }
           return element;
@@ -95,7 +96,7 @@ export const Portfolio = ({ arrayPortfolio }) => {
     }, []);
     setNewArray(newPortfolio);
   }, [arrayPortfolio]);
-
+  
   return (
     <Flex my={1} flexDir={"column"}>
       <Flex
@@ -173,7 +174,7 @@ export const Portfolio = ({ arrayPortfolio }) => {
                 Gan/Per {!defaultCheck ? "ARS" : "USD"}
               </Text>
               <Text w={36}>Gan/Per %</Text>
-              <Text w={36}>
+              <Text w={44}>
                 <strong>Total en {!defaultCheck ? "ARS" : "USD"}</strong>
               </Text>
             </Flex>
@@ -188,10 +189,8 @@ export const Portfolio = ({ arrayPortfolio }) => {
               p.name.includes(portfolio.name)
             );
             const currentPrice = filteredPrice[0]?.current_price;
-            const quantity = portfolio.quantity;
-            const total = quantity * currentPrice;
-
-            const profitLoss = total - quantity * portfolio.price;
+            const total = portfolio.currentTotal;
+            const profitLoss = currentPrice - portfolio.price;
             const profitLossPercentage =
               (currentPrice * 100) / portfolio.price - 100;
 
@@ -208,7 +207,7 @@ export const Portfolio = ({ arrayPortfolio }) => {
                     >
                       <Img w={[12, 12, 8, 8]} src={portfolio.image}></Img>
 
-                      <Text w={24} fontSize={"lg"}>
+                      <Text w={24} fontSize={"lg"} isTruncated>
                         {portfolio.name}
                       </Text>
                       <Text w={32}>{portfolio.quantity?.toLocaleString()}</Text>
@@ -225,7 +224,7 @@ export const Portfolio = ({ arrayPortfolio }) => {
                           ? `$${portfolio.price?.toLocaleString()}`
                           : `$${portfolio.price?.toLocaleString()}`}
                       </Text>
-                      <Text w={36} display={displayPreset}>
+                      <Text w={36} display={displayPreset} isTruncated>
                         {!defaultCheck
                           ? portfolio.currencySwitch
                             ? `$${(currentPrice * dolar)?.toLocaleString()}`
@@ -253,7 +252,7 @@ export const Portfolio = ({ arrayPortfolio }) => {
                           ? `$${profitLoss?.toLocaleString()}`
                           : `$${profitLoss?.toLocaleString()}`}
                       </Text>
-                      <Text w={36}>
+                      <Text w={36} isTruncated>
                         {profitLossPercentage !== 0 ? (
                           profitLossPercentage > 0 ? (
                             <Stat>
@@ -278,8 +277,7 @@ export const Portfolio = ({ arrayPortfolio }) => {
                           </Stat>
                         )}
                       </Text>
-
-                      <Text w={36}>
+                      <Text w={44} isTruncated>
                         {!defaultCheck ? (
                           portfolio.currencySwitch ? (
                             <FormattedArs ars={total * dolar} />
@@ -291,7 +289,6 @@ export const Portfolio = ({ arrayPortfolio }) => {
                         ) : (
                           <FormattedUsd usd={total} />
                         )}
-                        {}
                       </Text>
                     </Flex>
                     <Divider />
@@ -304,15 +301,20 @@ export const Portfolio = ({ arrayPortfolio }) => {
           })
         : ""}
 
-      {arrayPortfolio.length === 0 ? (
-        ""
-      ) : (
+
+      {arrayPortfolio.length > 0 && (
         <Flex
           flexDir={"column"}
           alignItems={["center", "center", "flex-end", "flex-end"]}
         >
-          <Text mt={1} w={36} fontSize="lg">
-            <strong>Total: {totalPortfolio.toFixed(2)} </strong>
+          <Text mt={1} w={44} isTruncated fontSize="lg"  fontWeight="bold">
+            <Flex>
+               {defaultCheck ? 
+           <FormattedUsd usd={totalPortfolio.toFixed(2)} /> : 
+           <FormattedArs ars={totalPortfolio.toFixed(2) * dolar} />
+            } 
+            </Flex>
+          
           </Text>
         </Flex>
       )}
